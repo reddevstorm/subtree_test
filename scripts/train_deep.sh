a3b97cb2bb19d1219a149541b30c34bae1d68c4d
luke <zzanzza@finger-plus.com>
2018-02-21 16:08:24 +0900

    bbox notebook 작업 중

--- scripts/train_deep.sh
+++ scripts/train_deep.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-python keras_retinanet/bin/train.py --batch-size 1 --epochs 30 --steps 10000 csv dataset/deepfashion/instances_train2017.csv dataset/deepfashion/categories2017.csv
+nohup cmd & python keras_retinanet/bin/train.py --batch-size 1 --epochs 10 --steps 50000 csv /Users/edwin/Documents/works/AI/datasets/deepfashion/instances_train2017.csv /Users/edwin/Documents/works/AI/datasets/deepfashion/categories2017.csv
+
